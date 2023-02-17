b2cb3766362cbc0fde352f135a2ad760e2d2d1f7
ji2902 <jminfo2902@gmail.com>
2023-02-17 06:49:15 +0000

    a

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -10,6 +10,9 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        int macks = nums .stream() .mapToInt(v -> v) .max() .orElseThrow();
+        int mynn = nums .stream() .mapToInt(v -> v) .min() .orElseThrow();
+        return macks - mynn;
     }
-}
+
+    }
